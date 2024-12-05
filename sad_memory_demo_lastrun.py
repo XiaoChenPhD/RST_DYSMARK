b7f5f0db81f1617523e4a060f29b56fed31142a9
Xiao Chen <137208320+XiaoChenPhD@users.noreply.github.com>
2024-12-05 20:26:17 +0800

    bug fixation

--- sad_memory_demo_lastrun.py
+++ sad_memory_demo_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.1.3),
-    on Wed Aug 21 11:25:48 2024
+    on Mon Oct 28 14:26:09 2024
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
